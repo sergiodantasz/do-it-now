8c8b968dc455e984d34f7dcd74c3c7fa9834aabf
Sérgio Dantas <sergiodnts828@gmail.com>
2024-02-16 09:55:13 -0300

    revert: change static files root
    This reverts commit 9836e07b0bc40c6f22a5786754c472205517a6f3.

--- core/settings.py
+++ core/settings.py
@@ -91,7 +91,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [BASE_DIR / 'base' / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
