c3e6cf87502c3d85086f162d3fd3f8c6813f1fb5
Sérgio Dantas <sergiodnts828@gmail.com>
2024-02-16 17:49:34 -0300

    chore: remove static url

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -8,5 +6,3 @@ urlpatterns = [
     path('', include('tasks.urls')),
     path('', include('users.urls')),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
