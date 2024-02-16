caef0a76ab931affcc8c0b9fa8761f9ec1ccde3e
Sérgio Dantas <sergiodnts828@gmail.com>
2024-02-16 12:37:51 -0300

    feat: configure models at admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'user']
+    list_display_links = ['id']
+    list_editable = ['name']
+    list_filter = ['user']
+    list_per_page = 15
+    search_fields = ['name', 'user']
+    ordering = ['-id']
--- tasks/models.py
+++ tasks/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Task(models.Model):
     name = models.CharField(unique=True, blank=False, max_length=250)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
