26c0cac33c83271014526e4c35fa73a66de12573
Sérgio Dantas <sergiodnts828@gmail.com>
2024-02-16 18:44:10 -0300

    feat: configure vercel deployment files

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
+
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+echo "Installing the packages..."
+python -m pip install -r requirements.txt
+
+echo "Applying migrations..."
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+
+echo "Collecting static files..."
+python manage.py collectstatic --no-input --clear
