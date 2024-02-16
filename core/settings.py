45fb3f896f77cbcdec06ec20efacf3b1cd956e18
Sérgio Dantas <sergiodnts828@gmail.com>
2024-02-16 18:21:15 -0300

    feat: configure static files

--- core/settings.py
+++ core/settings.py
@@ -88,7 +88,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 STATICFILES_DIRS = [BASE_DIR / 'base' / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
