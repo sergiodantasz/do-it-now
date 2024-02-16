ffeaf404a06b63a3e7d6dd8cb27e87c7bea73c86
Sérgio Dantas <sergiodnts828@gmail.com>
2024-02-16 16:39:19 -0300

    feat: configure django-admin-relation-links

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 
+from django_admin_relation_links import AdminChangeLinksMixin
+
 from .models import Task
 
 
 @admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'user']
+class TaskAdmin(AdminChangeLinksMixin, admin.ModelAdmin):
+    list_display = ['id', 'name', 'user_link']
     list_display_links = ['id']
     list_editable = ['name']
     list_filter = ['user']
     list_per_page = 15
     search_fields = ['name', 'user']
     ordering = ['-id']
+    change_links = ['user']
